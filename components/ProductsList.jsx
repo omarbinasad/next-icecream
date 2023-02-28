1f674ba65b8bb42b60196280c05a06866c30f171
limon9743 <limon9743@gmail.com>
2023-02-28 12:49:55 +0600

    filterable products

--- components/ProductsList.jsx
+++ components/ProductsList.jsx
@@ -23,7 +23,7 @@ const ProductsList = ({ products }) => {
   }
   return (
     <div>
-      <h1>Products</h1>
+      
       <div className="row px-5 px-md-0 row-cols-1 row-cols-md-3 row-cols-lg-3 g-4">
         {content}
       </div>
--- pages/shop.jsx
+++ pages/shop.jsx
@@ -4,6 +4,13 @@ import Banner from "../components/Banner";
 import FilterSection from "../components/FilterSection";
 import ProductsList from "../components/ProductsList";
 import { getAllProducts } from "../lib/productStore";
+// react-bootstrap tab
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import SortFilter from "../components/SortFilter";
 
 const Shop = ({ products }) => {
   return (
@@ -17,11 +24,25 @@ const Shop = ({ products }) => {
             </div>
           </div>
           <div className={`col-9`}>
-            <div className={`sort_filter`}>
-
-            </div>
             <div className={`products_list`}>
+              <Tab.Container id="uncontrolled-tab-example" defaultActiveKey="first">
+                <Col>
+                <div className={`sort_filter`}>
+                  <SortFilter/>
+                  </div>
+                  
+                  <Col >
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
             <ProductsList products={products} />
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <h1>Something</h1>
+            </Tab.Pane>
+          </Tab.Content>
+                  </Col>
+                </Col>
+              </Tab.Container>
             </div>
           </div>
         </div>
